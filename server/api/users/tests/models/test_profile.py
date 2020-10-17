92ea1585ad0692dd0f9fae504edd259aff0d3ee9
Jordan Kowal <kowaljordan@gmail.com>
2020-10-17 18:30:29 +0200

    [TEST] users: Added tests for the Profile model

--- server/api/users/tests/models/test_profile.py
+++ server/api/users/tests/models/test_profile.py
@@ -1,10 +1,14 @@
 """Tests for the 'Profile' model"""
 
+# Django
+from django.conf import settings
+from django.db import IntegrityError, transaction
+
 # Personal
 from jklib.django.db.tests import ModelTestCase
 
 # Local
-from ...models import Profile
+from ...models import Profile, Token
 
 
 # --------------------------------------------------------------------------------
@@ -14,7 +18,6 @@ class TestProfile(ModelTestCase):
     """Tests our Profile model which extends the django User model"""
 
     model_class = Profile
-    required_fields = ["user"]
 
     # ----------------------------------------
     # Behavior
@@ -25,9 +28,8 @@ class TestProfile(ModelTestCase):
         pass
 
     def setUp(self):
-        """Creates a user and an admin user"""
-        self.create_user()
-        self.create_admin_user()
+        """Not implemented"""
+        pass
 
     def tearDown(self):
         """Not implemented"""
@@ -42,25 +44,72 @@ class TestProfile(ModelTestCase):
     # Tests
     # ----------------------------------------
     def test_required_fields(self):
-        """WIP"""
-        pass
+        """Tests that you cannot create a Profile without a User"""
+        with transaction.atomic():
+            with self.assertRaises(IntegrityError):
+                self.model_class.objects.create(is_verified=False)
+        self.assert_instance_count_equals(0)
 
     def test_user_cascade_deletion(self):
-        """WIP"""
-        pass
+        """Tests that deleting a user also removes his Profile instance"""
+        user_1 = self.create_user()
+        self.create_user()
+        self.assert_instance_count_equals(2)
+        user_1.delete()
+        self.assert_instance_count_equals(1)
 
     def test_send_password_updated_email(self):
-        """WIP"""
-        pass
+        """Tests that the password updated email is sent correctly"""
+        subject = self.model_class.EMAILS["password_updated"]["subject"]
+        user = self.create_user()
+        user.profile.send_password_updated_email()
+        self.assert_email_was_sent(subject)
 
     def test_send_reset_password_email(self):
-        """WIP"""
-        pass
+        """Tests that the reset password email is sent correctly with a valid token"""
+        subject = self.model_class.EMAILS["request_password_reset"]["subject"]
+        user = self.create_user()
+        user.profile.send_reset_password_email()
+        token_type, _ = settings.RESET_TOKEN
+        self._assert_token_has_been_created(user, token_type)
+        self.assert_email_was_sent(subject)
 
     def test_send_verification_email(self):
-        """WIP"""
-        pass
+        """Tests that the verification email is sent only to unverified users, and includes a valid token"""
+        subject = self.model_class.EMAILS["verification_email"]["subject"]
+        user = self.create_user()
+        # Verified
+        user.profile.is_verified = True
+        user.profile.save()
+        user.profile.send_verification_email()
+        with self.assertRaises((AssertionError, IndexError)):
+            self.assert_email_was_sent(subject)
+        # Not verified
+        user.profile.is_verified = False
+        user.profile.save()
+        user.profile.send_verification_email()
+        token_type, _ = settings.VERIFY_TOKEN
+        self._assert_token_has_been_created(user, token_type)
+        self.assert_email_was_sent(subject)
 
     def test_send_welcome_email(self):
-        """WIP"""
-        pass
+        """Tests that the welcome email is sent correctly"""
+        subject = self.model_class.EMAILS["welcome"]["subject"]
+        user = self.create_user()
+        user.profile.send_welcome_email()
+        self.assert_email_was_sent(subject)
+
+    # ----------------------------------------
+    # Private
+    # ----------------------------------------
+    @staticmethod
+    def _assert_token_has_been_created(user, token_type):
+        """
+        Tests that there is ONE token in the database, and it matches with the user and type provided
+        :param User user: The User instance we sent an email to
+        :param str token_type: Type of the token, which depends on the email sent
+        """
+        token = Token.objects.get(user=user, type=token_type)
+        assert Token.objects.count() == 1
+        assert token.is_active_token
+        assert token.can_be_used
